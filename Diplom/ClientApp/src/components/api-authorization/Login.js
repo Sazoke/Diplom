9ea1bc85e73d6b4f4faf861cbf35a0f65a27acca
yCheems <64150828+yCheems@users.noreply.github.com>
2022-04-16 22:01:48 +0500

    NavMenu fix

--- Diplom/ClientApp/src/components/api-authorization/Login.js
+++ Diplom/ClientApp/src/components/api-authorization/Login.js
@@ -3,8 +3,6 @@ import { Component } from 'react';
 import authService from './AuthorizeService';
 import { AuthenticationResultStatus } from './AuthorizeService';
 import { LoginActions, QueryParameterNames, ApplicationPaths } from './ApiAuthorizationConstants';
-import { RegistrationForm  } from '../RegistrationForm/RegistrationFrom';
-import {Profile} from "../Profile/Profile";
 
 // The main responsibility of this component is to handle the user's login process.
 // This is the starting point for the login process. Any component that needs to authenticate
@@ -37,7 +35,7 @@ export class Login extends Component {
                 this.redirectToProfile();
                 break;
             case LoginActions.Register:
-                //this.redirectToRegister();
+                this.redirectToRegister();
                 break;
             default:
                 throw new Error(`Invalid action '${action}'`);
@@ -53,13 +51,12 @@ export class Login extends Component {
         } else {
             switch (action) {
                 case LoginActions.Login:
-                    return (<RegistrationForm />);
+                    return (<div>Processing login</div>);
                 case LoginActions.LoginCallback:
                     return (<div>Processing login callback</div>);
                 case LoginActions.Profile:
-                    return (<Profile />)
                 case LoginActions.Register:
-                    return (<RegistrationForm />);
+                    return (<div></div>);
                 default:
                     throw new Error(`Invalid action '${action}'`);
             }
